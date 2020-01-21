b89f5a07ff577bf45d224b465d802be704cf7030
idodela <i_delawar@hotmail.com>
2020-01-21 17:42:05 +0100

    recent

---
+++ src/app/components/upload-artwork/upload-artwork.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-artwork',
+  templateUrl: './upload-artwork.component.html',
+  styleUrls: ['./upload-artwork.component.css']
+})
+export class UploadArtworkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
