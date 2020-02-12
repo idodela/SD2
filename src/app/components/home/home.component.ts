f619a3b88c1189418213570e1b8b2a4be51256be
idodela <46161926+idodela@users.noreply.github.com>
2020-02-12 01:46:08 +0100

    versie 21

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  myimage1:string="assets/employee.jpg";
+  myimage2:string="assets/students.jpg";
+  myimage3:string="assets/future.jpg";
+
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
