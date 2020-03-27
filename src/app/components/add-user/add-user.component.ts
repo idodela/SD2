c4a2cd5a3e264b8b1a299a7c04b00d8eb51a2b01
idodela <46161926+idodela@users.noreply.github.com>
2020-03-27 22:23:35 +0100

    25-03

---
+++ src/app/components/add-user/add-user.component.ts
@@ -0,0 +1,78 @@
+import {Component, OnInit} from '@angular/core';
+// import {UserRoles} from '../user-details/user-details.component';
+import {FormControl, FormGroup} from '@angular/forms';
+import {Router} from '@angular/router';
+import {UserService} from "../../services/user.service";
+// import {UsersService} from '../../../services/users.service';
+// import {User} from '../../../models/user';
+
+@Component({
+  selector: 'app-add-user',
+  templateUrl: './add-user.component.html',
+  styleUrls: ['./add-user.component.css']
+})
+export class AddUserComponent implements OnInit {
+  private invalid = false;
+  private noneMatchingPasswords = false;
+  // private roles: UserRoles[] = [
+  //   new UserRoles('Runner', 'RUN'),
+  //   new UserRoles('Ground Engineer', 'GE'),
+  //   new UserRoles('Administrator', 'ADM')
+  // ];
+
+  private userForm = new FormGroup({
+    name: new FormControl(),
+    password: new FormControl(),
+    validatePassword: new FormControl(),
+    role: new FormControl()
+  });
+
+  constructor(private router: Router, private usersService: UserService) {
+  }
+
+  ngOnInit() {
+  }
+
+
+  onCancel() {
+    let confirmation = true;
+    if (this.userForm.dirty) {
+      confirmation = confirm('Are you sure? All changes will be discarded if you continue!');
+    }
+
+    if (confirmation) {
+    //   this.router.navigate(['../../users']);
+    // }
+  }
+
+  // onAddUser() {
+  //   this.invalid = false;
+  //   this.noneMatchingPasswords = false;
+
+    if (this.userForm.value.password !== this.userForm.value.validatePassword) {
+      this.noneMatchingPasswords = true;
+    }
+
+    if (!this.userForm.invalid && this.userForm.value.role !== null && !this.noneMatchingPasswords) {
+      const userValue = this.userForm.value;
+      // let roleAbbr = 'RUN';
+      // this.roles.forEach(role => {
+      //   if (role.role === userValue.role) {
+      //     roleAbbr = role.abbreviation;
+      //   }
+      // });
+      // const user = new User(userValue.name, roleAbbr, 'OFF', userValue.password);
+
+      // console.log(user);
+      // this.usersService.addUser(user);
+      // this.router.navigate(['../../users']);
+    } else {
+      if (!this.noneMatchingPasswords) {
+        this.invalid = true;
+      }
+    }
+  }
+
+
+
+}
